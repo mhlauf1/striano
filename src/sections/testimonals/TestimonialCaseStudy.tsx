a06275a8cbd7e888e19a186fbcf998b46a0ba3b4
mhlauf1 <mlaufersweiler@gmail.com>
2025-03-04 11:29:13 -0600

    removing main tag

--- src/sections/testimonals/TestimonialCaseStudy.tsx
+++ src/sections/testimonals/TestimonialCaseStudy.tsx
@@ -5,11 +5,9 @@ const projectImages = [{ id: 1, src: "/Citi2.jpg" }];
 
 export default function TestimonialsPage() {
   return (
-    <main>
-      <TestimonialSection
-        testimonials={testimonialData}
-        projectImages={projectImages}
-      />
-    </main>
+    <TestimonialSection
+      testimonials={testimonialData}
+      projectImages={projectImages}
+    />
   );
 }
