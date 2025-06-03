77fe71de2a4ebb8221afcecf9ea5234d46f34459
mhlauf1 <mlaufersweiler@gmail.com>
2025-06-03 13:12:46 -0500

    updating metadata:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import Nav from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -51,7 +50,6 @@ export default function RootLayout({
       <body className={`${inter.variable} overflow-x-hidden antialiased`}>
         <Nav />
         {children}
-        <Analytics />
         <Footer />
       </body>
     </html>
